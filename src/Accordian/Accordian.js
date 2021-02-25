423649a09b5585aa9a0cd8df959323a20160c43d
Victor Holguin <vic@vics.pics>
2021-02-25 10:03:36 -0800

    accordian adjusts height if child content height increases

--- src/Accordian/Accordian.js
+++ src/Accordian/Accordian.js
@@ -1,42 +1,47 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useSpring, animated }  from 'react-spring';
 import PlaceholderBtn from '../PlaceholderBtn';
+import { useResizeDetector } from 'react-resize-detector';
+import useResizeAware from 'react-resize-aware';
+import useHeight  from '../useHeight';
 import './Accordian.scss'
 
 export default function Accordian({ children, expander: Expander, footer: Footer, header: Header, opened = false }) {
 	let defaultHeight = 0;
 	const [expanded, toggle] = useState(opened);
 	const [contentHeight, setContentHeight] = useState(defaultHeight);
-	const contentRef = useRef(null);
+	const [resizeListener, sizes] = useResizeAware();
 	const expand = useSpring({
 		config: { friction: 50, tension: 350 },
-		height: expanded ? `${contentHeight}px` : `${defaultHeight}px`
+		from: { height: 0 },
+		to: {height: expanded ? contentHeight : 0}
 	});
 
 	const openClose = () => {
 		if (expanded) document.activeElement.blur();
 		toggle(!expanded);
 	};
+
 	const switchProps = {
 		active: expanded,
 		onClick: openClose
 	};
 
 	useEffect(() => {
-		setContentHeight(contentRef.current.scrollHeight); //Sets initial height	
-		window.addEventListener("resize", setContentHeight(contentRef.current.scrollHeight)); //Adds resize event listener
-		return window.removeEventListener("resize", setContentHeight(contentRef.current.scrollHeight)); // Clean-up
-	}, [contentRef]);
+		if (sizes.height) setContentHeight(sizes.height);
+	});
 
 	return(
 		<div className='stuff-accordian-container' >
 			<header>
 				{Header ? <Header /> : ''}
+				{sizes.height}
 				{Expander ? <Expander {...switchProps} /> : <PlaceholderBtn {...switchProps} />}
 			</header>
-			<animated.div className='stuff-accordian' style={expand}>
-				<div className='stuff-accordian-content' ref={contentRef}>
+			<animated.div className='stuff-accordian' style={expand} >
+				<div className='stuff-accordian-content'>
 					<hr />
+					{resizeListener}
 					{children}
 					<footer>
 						{Footer ? <Footer /> : ''}
---
+++ src/useMeasure.js
@@ -0,0 +1,13 @@
+import { useRef, useState, useEffect } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
+
+export default function useMeasure() {
+	const ref = useRef();
+	const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 });
+	const [ro] = useState(() => new ResizeObserver(([entry]) => {
+		console.log(entry);
+		set(entry.contentRect)
+	}));
+	useEffect(() => (ro.observe(ref.current), ro.disconnect), []);
+	return [{ ref }, bounds];
+};
