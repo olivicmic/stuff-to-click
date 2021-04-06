53a71579ac2645d478b8123180f8f9484e56fbb7
Victor Holguin <vic@vics.pics>
2021-04-06 16:44:08 -0700

    SlideInput extra component can be passed attributes from the main component via the extraAttr attribute

--- src/SlideInput/SlideInput.js
+++ src/SlideInput/SlideInput.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
 import SlideWrapper  from '../SlideWrapper';
-const Input = ({focus, extra: Extra, type = 'text', valueName, ...rest}) => 
+const Input = ({focus, extra: Extra, extraAttr, type = 'text', valueName, ...rest}) => 
 <div className='stuff-slide-input-content'>
 	<input type={type} {...rest}/>
-	{ Extra ? <Extra className='stuff-slide-input-extra'/> : null }
+	{ Extra ? <Extra extraAttr={extraAttr} className='stuff-slide-input-extra'/> : null }
 </div>;
 
 const SlideInput = ({ ...rest}) => {
