75bce691029e20d4211775cbbe071ab630fbabc1
olivicmic <vic@vics.pics>
2022-08-24 22:08:51 -0700

    added onFocus/onBlur passhtrough on input

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -8,7 +8,7 @@ const Input = ({ events, extra: Extra, extraAttr, focus, type = 'text', valueNam
 		{ Extra ? <Extra {...extraAttr} className='stuff-input-extra'/> : null }
 	</div>;
 
-export default function SlideInput({ ...rest }) {
-	const { events, focus } = useInput({});
+export default function SlideInput({ onBlur, onFocus, ...rest }) {
+	const { events, focus } = useInput({ onBlur, onFocus });
 	return <InputWrap { ...{ ...rest, events, focus, component: Input } } />;
 };
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 const b = () => {};
 
-export default function useInput({ close = b, click = b, during = b, debug, hostid }) {
+export default function useInput({ close = b, click = b, during = b, debug, hostid, onBlur = b, onFocus = b }) {
 	const [focus, setFocus] = useState(false);
 	return {
 		events: {
 			onBlur: e => {
+				onBlur(e);
 				let targetID = e?.relatedTarget?.attributes?.hostid?.value;
 				if (debug) console.log({location: 'stuff useInput onBlur', targetID, hostid});
 				if (!hostid) setFocus(false);
@@ -17,6 +18,7 @@ export default function useInput({ close = b, click = b, during = b, debug, host
 				click(e);
 			},
 			onFocus: e => {
+				onFocus(e);
 				if (debug) console.log({location: 'stuff useInput onFocus', e});
 				setFocus(true);
 				during(e); 
