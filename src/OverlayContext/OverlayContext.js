1cdaef0616e56623542404f04639564dc2b4a33e
olivicmic <vic@vics.pics>
2022-09-03 12:15:43 -0700

    added modal state to useOverlay

--- src/OverlayContext/OverlayContext.js
+++ src/OverlayContext/OverlayContext.js
@@ -2,9 +2,14 @@ import { createContext } from 'react';
 
 const f = () => {};
 export default createContext({
+	addModal: f,
 	addOverlay: f,
+	deleteModal: f,
 	deleteOverlay: f,
+	modals: [],
+	modalState: [],
 	overlays: [],
 	overState: [],
+	updateModal: f
 	updateOverlay: f
 });
