37f4b5cdec9d14c298ef4f1b16f53115b401d248
olivicmic <vic@vics.pics>
2023-03-09 16:19:26 -0800

    maybe better resizing?

--- src/dev/ExampleSlide/ExampleSlide.js
+++ src/dev/ExampleSlide/ExampleSlide.js
@@ -7,7 +7,6 @@ export default function ExampleSlide({ busy, num, pagination, value, ...more }) 
 		<button onClick={pagination.back} disabled={pagination.atStart || outBusy}>Back</button>
 		<button onClick={pagination.forward} disabled={pagination.atEnd || outBusy}>Next</button>
 		<div>
-			<div>{ outBusy && 'busy' }</div>
 			<div>{ pagination.page + ': ' + value }</div>
 			{ pagination.page === 2 ? <textarea className='ex-slide-txt' cols="30" rows="10" /> : null}
 			<input type='number' value={reqPg} onChange={e => setReqPg(e.target.value)} disabled={outBusy}/>
--- src/lib/Slide/Slide.js
+++ src/lib/Slide/Slide.js
@@ -1,26 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { animated } from 'react-spring';
+import { useResizeDetector } from 'react-resize-detector';
 import { is }  from 'lal';
 
 export default function Slide({ body: Body, debug, className, pagination, slideState, volume, volumeSet, style, ...rest }) {
 	const height = is.defined(volume.height, 0);
 	const width = is.defined(volume.width, 0);
-	const ref = useRef();
+	const { height: newHeight, width: newWidth, ref } = useResizeDetector();
 
 	useEffect(() => {
-		const main = { width: ref?.current?.clientWidth || 0, height: ref?.current?.clientHeight || 0 };
-		
 		if (pagination.isCurrent) {
-			let newHeight = main.height;
-			let newWidth = main.width;
-
-			if (height !== newHeight) volumeSet({ width, height: newHeight 	}); 
+			if (height !== newHeight) volumeSet({ width, height: newHeight });
 			if (width !== newWidth) volumeSet({	height,	width: newWidth });
-			
 		}
-	},[debug, height, pagination.isCurrent, ref, volume, volumeSet, width]);
+	},[debug, height, newHeight, newWidth, pagination.isCurrent, volume, volumeSet, width]);
 
-	return <animated.div { ...{ className: `stuff-slideshow-slide${ className ? ' ' + className : '' }`, ref, style } }>
+	return <animated.div { ...{ className: `stuff-slideshow-slide${ className ? ' ' + className : '' }`, ref, style: { ...style, width: 'min-content' } } }>
 		{ Body && <Body { ...{...rest, pagination, ...slideState } } /> }
 	</animated.div>;
 };
